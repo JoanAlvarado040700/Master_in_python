7d6280243f78da5a4bf682ae147c83e78adf2f5a
JoanAlvarado040700 <sevaslarios@gmail.com>
2022-01-30 01:47:58 -0600

    MYSQL

---
+++ 19-bases-de-datos/main.py
@@ -0,0 +1,60 @@
+
+import mysql.connector
+
+#Conexion
+
+database = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "master"
+)
+
+
+cursor = database.cursor()
+
+
+#Crear tablas 
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS vehiculos(
+    id int(10) auto_increment not null,
+    marca varchar(40) not null,
+    modelo varchar(40) not null,
+    precio float(10,2) not null,
+    CONSTRAINT pk_vehiculo PRIMARY KEY(id)
+
+)
+"""
+)
+
+#Insertar datos en una tabla 
+'''
+
+cursor.execute("INSERT INTO vehiculos VALUES(null,'opel','astra',1723.4 ) ")
+database.commit()
+
+'''
+
+#Insertar de manera masiva 
+
+'''
+
+coches = [
+
+    ("seat","ibiza",50000),
+    ("Renault","Saxo",35000),
+    ("mercedes","Clase C",24000)
+
+]
+
+cursor.executemany("INSERT INTO vehiculos VALUE(null,%s,%s,%s)", coches)
+database.commit()
+'''
+
+
+cursor.execute("SELECT * FROM vehiculos ")
+
+resul = cursor.fetchall()
+
+for coche in resul:
+    print(coche)
